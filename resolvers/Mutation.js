5bdc9b0da66eabe68eeea6aece171a75c260730e
Guilherme de Paula Santos <guilherme.paula@TIT-1523.local>
2019-09-26 15:07:53 -0300

    Inputs

--- resolvers/Mutation.js
+++ resolvers/Mutation.js
@@ -2,14 +2,14 @@ const {usuarios, proximoId} = require('../data/db')
 
 module.exports = {
     // { nome, email, idade }
-    novoUsuario(_, args){
-        const emailExistente = usuarios.some(u => u.email === args.email)
+    novoUsuario(_, { dados }){
+        const emailExistente = usuarios.some(u => u.email === dados.email)
         if (emailExistente) {
             throw new Error('Email já cadastrado')
         }
         const novo = {
             id: proximoId(),
-            ...args,
+            ...dados,
             perfil_id: 1,
             status: 'MORTO'
         }
@@ -22,7 +22,7 @@ module.exports = {
         const excluidos = usuarios.splice(i, 1)
         return excluidos ? excluidos[0] : null
     },
-    alterarUsuario(_, args){
+    alterarUsuario(_, args ){
         const i = usuarios.findIndex(u => u.id == args.id)
         if (i < 0) throw new Error('Usuario não existe')
         const usuario = {
