e2590fe1bbe6ed160195433bb9b0bcb6e5661457
ZoeiroVader <vortexgamedevs@gmail.com>
2019-09-29 15:53:45 -0300

    knex implementation

---
+++ banco-de-dados/knexfile.js
@@ -0,0 +1,17 @@
+// Update with your config settings.
+
+module.exports = {
+  client: 'mysql',
+  connection: {
+    database: 'graphQL',
+    user:     'root',
+    password: '1234567'
+  },
+  pool: {
+    min: 2,
+    max: 10
+  },
+  migrations: {
+    tableName: 'knex_migrations'
+  }
+};
