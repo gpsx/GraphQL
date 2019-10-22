41fdffc43ccf9fdd7851c94a8751655d48292bf9
Guilherme de Paula Santos <guilherme.paula@TIT-1523.local>
2019-10-22 14:24:22 -0300

    projeto backend

---
+++ backend/knexfile.js
@@ -0,0 +1,25 @@
+// Update with your config settings.
+
+module.exports = {
+  // client: 'mysql',
+  // connection: {
+  //   database: 'desafiocap04',
+  //   user:     'root',
+  //   password: 'bandtec'
+  // },
+  client: 'mssql',
+  connection: {
+    host: 'servidor01191045.database.windows.net',
+    user: 'GF01191045',
+    password: '#Gf46768871838',
+    database: 'RaiseAdventurer',
+    encrypt: true
+  },
+  pool: {
+    min: 2,
+    max: 10
+  },
+  migrations: {
+    tableName: 'knex_migrations'
+  }
+};
